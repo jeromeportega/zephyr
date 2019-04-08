05669fceac234f433f547e79405736882d3d2d86
Jerome Ortega <jeromeortega@Jeromes-MacBook-Pro-2.local>
2019-04-08 18:28:31 -0500

    abstract routes to other files based on path

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,23 @@
 const express = require('express');
-import * as next from 'next'
+import * as next from 'next';
 
-const port: Number = parseInt(<string>process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port: Number = parseInt(<string>process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const profileRoutes = require('./routes/profile');
 
 app.prepare()
   .then(() => {
     const server = express();
 
-    server.get('/profile/:id', (req, res) => {
-      /**
-       * How we're going to be showing different pages on the same route conditionally.
-       */
-      if (req.params.id !== 'andersblom' && req.params.id !== 'jerryortega') {
-        return app.render(req, res, '/profile/notFound', { id: req.params.id });  
-      }
-      return app.render(req, res, '/profile', { id: req.params.id });
-    });
+    server.use(express.json());
 
+    // Import Routes
+    profileRoutes({ server, app });
+
+    // Root route
     server.get('*', (req, res) => {
       return handle(req, res);
     });
@@ -28,4 +26,4 @@ app.prepare()
       if (err) throw err;
       console.log(`> Ready on http://localhost:${port}`);
     });
-  })
+  });
